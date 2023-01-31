d7c732bd730f55cd0052d8b791d8119cc8b34a06
abraham5175 <112706082+abraham5175@users.noreply.github.com>
2023-01-31 15:34:48 -0500

    Add files via upload

---
+++ Puzzle_1.sql
@@ -0,0 +1,26 @@
+CREATE TABLE lag
+(
+BusinessEntityID INT
+,SalesYear   INT
+,CurrentQuota  DECIMAL(20,4)
+)
+GO
+ 
+INSERT INTO lag
+SELECT 275 , 2005 , '367000.00'
+UNION ALL
+SELECT 275 , 2005 , '556000.00'
+UNION ALL
+SELECT 275 , 2006 , '502000.00'
+UNION ALL
+SELECT 275 , 2006 , '550000.00'
+UNION ALL
+SELECT 275 , 2006 , '1429000.00'
+UNION ALL
+SELECT 275 , 2006 ,  '1324000.00'
+
+
+select * from lag
+
+
+select BusinessEntityID, SalesYear,CurrentQuota, isnull(lag (CurrentQuota,1) over (order by BusinessEntityID),0) as lag_data from lag
